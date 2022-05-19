4aba210ae93da7e5dae6351d7b3dee2dd427d2e5
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-19 15:15:25 +0530

    Login Model adding field name

---
+++ EcommerceApp/View/LoginScreen.swift
@@ -0,0 +1,20 @@
+//
+//  LoginScreen.swift
+//  EcommerceApp
+//
+//  Created by Bhumika Patel on 19/05/22.
+//
+
+import SwiftUI
+
+struct LoginScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginScreen()
+    }
+}
---
+++ EcommerceApp/ViewModel/LoginScreenModel.swift
@@ -0,0 +1,32 @@
+//
+//  LoginScreenModel.swift
+//  EcommerceApp
+//
+//  Created by Bhumika Patel on 19/05/22.
+//
+
+import SwiftUI
+
+class LoginScreenModel: ObservableObject {
+    // Login Properties
+    @Published var email: String = ""
+    @Published var password: String = ""
+    @Published var showPassword: Bool = false
+    
+    // Register Properties
+    @Published var re_Enter_Password: String = ""
+    @Published var showReEnterPassword: Bool = false
+    
+    //Login call
+    func Login(){
+        // action
+    }
+    
+    func Register(){
+        // action
+    }
+    
+    func ForgotPassword(){
+       //action
+    }
+}
