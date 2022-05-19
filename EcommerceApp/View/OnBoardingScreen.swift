06c26c1c0d6389d6f9fe93fa3a1faf2bd37c95e1
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-19 14:21:06 +0530

    Add fonts and create new screen and call content view and background colour set

---
+++ EcommerceApp/View/OnBoardingScreen.swift
@@ -0,0 +1,26 @@
+//
+//  OnBoardingScreen.swift
+//  EcommerceApp
+//
+//  Created by Bhumika Patel on 19/05/22.
+//
+
+import SwiftUI
+
+struct OnBoardingScreen: View {
+    var body: some View {
+        VStack{
+            
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            Color("Purple")
+        )
+    }
+}
+
+struct OnBoardingScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        OnBoardingScreen()
+    }
+}
