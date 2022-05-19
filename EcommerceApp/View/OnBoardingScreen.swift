29b4d83c4abbabf1ad7e5bd5720f62e21245bcc5
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-19 14:59:46 +0530

    set background colour for image related and set all effected properties and preview device call

--- EcommerceApp/View/OnBoardingScreen.swift
+++ EcommerceApp/View/OnBoardingScreen.swift
@@ -12,13 +12,38 @@ import SwiftUI
 let customFont = "Raleway-Regular"
 struct OnBoardingScreen: View {
     var body: some View {
-        VStack{
+        VStack(alignment: .leading){
             Text("Find your\nGadget")
                 .font(.custom(customFont, size: 55))
+            //added all three font used
+                .fontWeight(.bold)
+                .foregroundColor(Color("Purple"))
+            Image("ob1")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Button{
+                
+            } label: {
+                Text("Get started")
+                    .font(.custom(customFont, size: 18))
+                    .fontWeight(.semibold)
+                    .padding(.vertical,18)
+                    .frame(maxWidth:.infinity)
+                    .background(Color("Purple"))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .shadow(color: Color.black.opacity(0.1), radius: 5, x: 5, y: 5)
+            }
+            .padding(.horizontal,10)
+            // add small distance for larger display
+            .offset(y:20)
+            
+            Spacer()
         }
+        .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(
-            Color("Purple")
+            Color("white")
         )
     }
 }
@@ -26,5 +51,9 @@ struct OnBoardingScreen: View {
 struct OnBoardingScreen_Previews: PreviewProvider {
     static var previews: some View {
         OnBoardingScreen()
+            .previewDevice("iPhone1")
+        OnBoardingScreen()
+            .previewDevice("iPhone12")
+        
     }
 }
