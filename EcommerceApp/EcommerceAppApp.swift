ac07f3ababc409eafcbbd6abc753af68aa039f7a
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-19 13:38:47 +0530

    Initial Commit

---
+++ EcommerceApp/EcommerceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcommerceAppApp.swift
+//  EcommerceApp
+//
+//  Created by Bhumika Patel on 19/05/22.
+//
+
+import SwiftUI
+
+@main
+struct EcommerceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
