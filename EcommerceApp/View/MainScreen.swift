88185ed881780eeb6ed5712af2d00cd7551d7b47
bhumikapatel251 <102209491+bhumikapatel251@users.noreply.github.com>
2022-05-21 13:29:14 +0530

    tab images size set and Clicking process

--- EcommerceApp/View/MainScreen.swift
+++ EcommerceApp/View/MainScreen.swift
@@ -10,18 +10,44 @@ import SwiftUI
 struct MainScreen: View {
     // current tab
     @State var currentTab: Tab = .Home
+    //Hidding tabbar
+    init(){
+        UITabBar.appearance().isHidden = true
+    }
+    
     var body: some View {
        //Tab View
-        TabView(selection: $currentTab) {
-            Text("Home")
-                .tag(Tab.Home)
-            Text("Like")
-                .tag(Tab.Like)
-            Text("Profile")
-                .tag(Tab.Profile)
-            Text("Cart")
-                .tag(Tab.Cart)
-            
+        VStack(spacing: 0) {
+            TabView(selection: $currentTab) {
+                Text("Home")
+                    .tag(Tab.Home)
+                Text("Like")
+                    .tag(Tab.Like)
+                Text("Profile")
+                    .tag(Tab.Profile)
+                Text("Cart")
+                    .tag(Tab.Cart)
+                
+            }
+            // custom tabbar
+            HStack(spacing: 0){
+                ForEach(Tab.allCases,id: \.self){tab in
+                    Button{
+                        // updating tab
+                        currentTab = tab
+                    }label: {
+                        Image(tab.rawValue)
+                            .resizable()
+                            .renderingMode(.template)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width:22, height: 22)
+                            .frame(maxWidth:.infinity )
+                            .foregroundColor(currentTab == tab ? Color("Purple1") : Color.black.opacity(0.3))
+                    }
+                }
+            }
+            .padding([.horizontal,.bottom])
+            .padding(.bottom,10)
         }
     }
 }
@@ -32,10 +58,10 @@ struct MainScreen_Previews: PreviewProvider {
     }
     
 }
-enum Tab: String {
+enum Tab: String,CaseIterable {
     // row value is image name is asset
-    case Home = "Home"
+    case Home = "Home1"
     case Like = "Like"
-    case Profile = "Profile"
+    case Profile = "Profile1"
     case Cart = "Cart"
 }
